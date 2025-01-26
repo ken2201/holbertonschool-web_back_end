8b0d1009d232943c571783aec490d5c67336146a
ken2201 <ndayishimiyeken22@gmail.com>
2025-01-26 21:46:24 +0100

    fixing

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,21 @@
+// Set the encoding for the standard input to UTF-8
 process.stdin.setEncoding('utf8');
 
+// Prompt the user for their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Listen for data input from the user
 process.stdin.on('readable', () => {
+  // Read the input from the user
   const name = process.stdin.read();
   if (name) {
+    // Display the user's name
     process.stdout.write(`Your name is: ${name}`);
   }
 });
 
+// Listen for the end of the input stream
 process.stdin.on('end', () => {
+  // Inform the user that the software is closing
   process.stdout.write('This important software is now closing\n');
 });
